b0598bade979ab2b394ee18c3bf6369613f99588
Dany <dany.bismuth@live.com>
2020-09-15 12:12:15 +0200

    week6day1

---
+++ Week6/Day1/ExerciseXP.py
@@ -0,0 +1,46 @@
+# Exercise 1
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+#print("Hello World")
+
+# Exercise 2
+# print((99^3)*8)
+
+# Exercise 3
+# name = "Dany"
+# age = 29
+# shoe_size = 44
+# info = (f"I'am {name} {age}yrs old My foot is so tall {shoe_size} is my shoe size")
+#
+# print(info)
+
+# Exercise 4
+# computer_brand="mac"
+# print(f"I have a {computer_brand} computer")
+
+# Exercise 5
+# print(5<3)
+# print(3==3)
+# print(3=="3")
+# # print("3">3)
+# print("Hello"=="hello")
+
+# Exercise 6
+# user_size=input("how tall are you, enter your size in Inches")
+# user_size= int(user_size)
+#
+# if user_size < 35:
+#     print("You need to drink more soup and grow a bit more")
+# else:
+#     print("Enjoy the ride")
+
+# Exercise 7
+# number=input("Please enter a number")
+# number=int(number)
+#
+# if number%2:
+#     print(f"{number} is odd")
+# else:
+#     print(f"{number} is even")
+
